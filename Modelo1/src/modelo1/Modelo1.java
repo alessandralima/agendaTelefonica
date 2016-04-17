92c43a9c3b13cba2ce902315f4a19a1a10914871
alesa <alesa@192.168.0.10>
2016-04-17 18:39:28 -0300

    Primeiro Teste no NetBeans + Git

--- Modelo1/src/modelo1/Modelo1.java
+++ Modelo1/src/modelo1/Modelo1.java
@@ -1,24 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo1;
 
-/**
- *
- * @author alesa
- */
 public class Modelo1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        System.out.println("\n"+adicionarPontos());
         System.out.println(retornaFrase());
+        System.out.println("\n"+adicionarPontos());
     }
     
     public static String retornaFrase() {
         return "Olá Mundo";
+    }
+    
+    public static String adicionarPontos() {
+        return "....................";
     }
 }
